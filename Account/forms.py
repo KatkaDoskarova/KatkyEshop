036deb341acea43c1cad891b78043bc382c1efe0
KatkaDoskarova <firefly3367@gmail.com>
2022-01-29 09:07:50 +0100

    doladění
    hlavní kod v base, upraveny scripty, upraven kod - best practices

--- Account/forms.py
+++ Account/forms.py
@@ -1,10 +1,14 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth import login, authenticate
+
 
 class RegistrationForm(UserCreationForm):
     email = forms.EmailField(required=True)
+    city = forms.CharField()
+    street = forms.CharField()
+    zipcode = forms.CharField()
+
 
     class Meta:
         model = User
@@ -15,17 +19,23 @@ class RegistrationForm(UserCreationForm):
             'password1',
             'password2',
             'email',
-
+            'city',
+            'street',
+            'zipcode',
         ]
+        help_texts = {"username": "Jmeno", "first_name": "Krestni jmeno"}
 
-    def save(self, comit=True):
+    def save(self, commit=True):
         user = super(RegistrationForm, self).save(commit=False)
+        user.username = self.cleaned_data['username']
         user.first_name = self.cleaned_data['first_name']
         user.last_name = self.cleaned_data['last_name']
         user.email = self.cleaned_data['email']
+        user.city = self.cleaned_data['city']
+        user.street = self.cleaned_data['street']
+        user.zipcode = self.cleaned_data['zipcode']
 
-        if comit:
+        if commit:
             user.save()
 
         return user
-
